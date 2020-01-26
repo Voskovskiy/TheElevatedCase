3791c52444bf49774fae72fcf56b0563d81dbcb2
Konstyantin <50234775+Voskovskiy@users.noreply.github.com>
2020-01-26 17:22:24 +0200

    Corrected console output order

--- src/main.kt
+++ src/main.kt
@@ -6,7 +6,7 @@ const val _maxRandom = 100
 var _elevatorCapacity = ranDom()
 var _buildingSize = ranDom()
 // Main method
-fun main(args: Array<String>) {
+fun main() {
     println("Welcome to the Elevated Case")
     separator()
     val elevator = Elevator()
@@ -14,18 +14,22 @@ fun main(args: Array<String>) {
     var turns = 0
     while (true) {
         if (turns != 0) {
-            println("Flour #${elevator.currentFlourIndex + 1}:")
-            val peopleLeftToDeliver = building.inside()
-            if (peopleLeftToDeliver > 0) println("People waiting for the elevator: $peopleLeftToDeliver")
-            println("Elevator: ${elevator.inside()}")
             separator()
+            if (building.isEmpty() and (elevator.isEmpty())) {
+                break
+            }   else {
+                println("Flour #${elevator.currentFlourIndex + 1}:")
+            }
         }
-        if (building.isEmpty() and (elevator.isEmpty())) break      // Job is done!
         if (!elevator.isEmpty()) {
-            val transfer = elevator.askAround()                     // Asking around if anyone arrived to this flour
+            val transfer = elevator.askAround()  // Asking around if anyone arrived to this flour
             if (transfer.isNotEmpty())
-                elevator.unload(transfer)    // Unloading if we have anyone to leave
+                elevator.unload(transfer)        // Unloading if we have anyone to leave
         }
+        val peopleInsideTheElevator = elevator.inside()
+        if (peopleInsideTheElevator > 0) println("People inside the elevator: $peopleInsideTheElevator")
+        val peopleLeftToDeliver = building.inside()
+        if (peopleLeftToDeliver > 0) println("People waiting for the elevator: $peopleLeftToDeliver")
         if (building.currentFlourIsEmpty(elevator.currentFlourIndex) or elevator.isFull()) {
             elevator.next()
             turns++
